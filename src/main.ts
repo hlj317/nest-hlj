3a991b8e5a80a1a2d24335e5629b87a47450fea4
hlj317 <huanglijun317@126.com>
2022-08-24 22:42:33 +0800

    调整页面渲染和静态资源目录加载结构

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ import * as ejsMate from 'ejs-mate';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const env = process.env.NODE_ENV || 'development';
   // 设置静态资源目录
-  app.useStaticAssets(join(__dirname,'..','vue/dist'));
+  if(env === "development"){
+    app.useStaticAssets(join(__dirname,'..','vue/dist'));
+  }else{
+    app.useStaticAssets(join(__dirname,'..','../vue/dist'));
+  }
   // 获取根目录 nest-cnode
   const rootDir = join(__dirname, '..');
   // 指定视图引擎 处理.html后缀文件
@@ -15,7 +20,11 @@ async function bootstrap() {
   // 视图引擎
   app.set('view engine', 'html');
   // 配置模板（视图）的基本目录
-  app.setBaseViewsDir(join(rootDir, 'vue/dist/pages'));
+  if(env === "development"){
+    app.setBaseViewsDir(join(rootDir, 'vue/dist/pages'));
+  }else{
+    app.setBaseViewsDir(join(rootDir, '../vue/dist/pages'));
+  }
   await app.listen(3000);
 }
 bootstrap();
